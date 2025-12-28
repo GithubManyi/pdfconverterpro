d36d32612ae20925d3bc89591acd243be5cae802
GithubManyi <manyigeoffrey3@gmail.com>
2025-12-29 01:22:40 +0300

    Fix static files error: css/style.css missing

--- core/settings.py
+++ core/settings.py
@@ -156,7 +156,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # ============ MEDIA FILES ============
 MEDIA_URL = '/media/'
